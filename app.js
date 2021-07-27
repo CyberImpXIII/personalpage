01b5cec08da8d1dd3936e59ce09a2e6554b64026
CyberImpXIII <jnelsenepstein@gmail.com>
2021-07-27 17:05:29 -0400

    tweaked

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const dir = `${__dirname}/public/`;
 
-app.get('/', (req, res) => {
-  res.send(index.html)
-})
+app.get("*", (req, res) => {
+  res.sendFile(dir + "index.html");
+});
+
+app.listen(3000);
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
